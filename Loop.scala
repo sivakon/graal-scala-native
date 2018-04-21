db98a48051fb4b53047d12878865362fbec03f70
Sivaram Konanki <sivaram.konanki@p45535.intra.rakuten.co.jp>
2018-04-21 15:34:35 +0900

    Bytecode stuff

---
+++ Loop.scala
@@ -0,0 +1,5 @@
+object Loop {
+  def main(args: Array[String]): Unit = {
+    for (i <- (0 until 10)) println(i)
+  }
+}
---
+++ LoopMap.scala
@@ -0,0 +1,5 @@
+object LoopMap {
+  def main(args: Array[String]): Unit = {
+    (0 until 10) foreach println
+  }
+}
